1c7f985c7f8c229bcf68eaa4eca9e25b0a6a7387
sathiskumar s <sathiskumarsaravanan243214@gmail.com>
2023-03-28 19:17:34 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Sathis</title>
+	<link rel="stylesheet" href="css/style.css?<?php echo time();?>">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous"> -->
+	<link href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap" rel="stylesheet">
+</head>
+
+<body>
+	<div class="top-container">
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Projects</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+		<img class="top-cloud" src="images/cloud.png" alt="cloud image">
+		<h1>I'm Sathis</h1>
+		<h2>a Fullstack Developer</h2>
+		<img class="bottom-cloud" src="images/cloud.png" alt="cloud image">
+		<img src="images/land.svg" alt="mountain image" class="land-image">
+	</div>
+	<div class="middle-container">
+		<div class="profile">
+			<img src="images/myimage.jpg" alt="my image" class="my-image">
+			<div class="about">
+				<h2 class="about-head">About</h2>
+				<p class="about-content">This is sathis. I am a fullstack web developer in past one year. Experienced developer with expert skills in HTML, CSS, JavaScript, Python, PHP and various frameworks who is eager to transition into a junior full-stack developer role. Excited to leverage my deep expertise in development and data management to create scalable, data-driven applications for DoorLoop.</p>
+			</div>
+		</div>
+		<hr>
+		<div class="skills">
+			<div class="skills-section">
+				<h2 class="skills-head">About</h2>
+				<p class="skills-content">This is sathis. I am a fullstack web developer in past one year. Experienced developer with expert skills in HTML, CSS, JavaScript, Python, PHP and various frameworks who is eager to transition into a junior full-stack developer role. Excited to leverage my deep expertise in development and data management to create scalable, data-driven applications for DoorLoop.</p>
+			</div>
+			<img src="images/myimage.jpg" alt="my image" class="skills-image">
+		</div>
+		<hr>
+		<div class="contact-me">
+			<h2>Get In Touch</h2>
+			<h3>Lorem ipsum dolor sit amet, non elit.</h3>
+			<p>Lorem ipsum dolor sit amet, in quis, aenean amet. Phasellus sodales, tellus donec dui, ornare erat.</p>
+			<a class="btn" href="mailto:name@email.com">CONTACT ME</a>
+		</div>
+	</div>
+
+
+	<div class="bottom-container">
+		<a class="footer-link" href="https://www.linkedin.com/">LinkedIn</a>
+		<a class="footer-link" href="https://twitter.com/">Twitter</a>
+		<a class="footer-link" href="#">Website</a>
+		<p>© Sathis - 2023</p>
+	</div>
+
+</body>
+
+</html>
